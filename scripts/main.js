57870dd72e71ade58809177e1db1af7d40e1b22a
Sharynne Azhar <sharynneazhar@gmail.com>
2016-09-09 14:16:58 -0500

    cleanup code begin to load calendars dynamically

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+/*
+  Created by Sharynne Azhar - 9/9/2016
+  Dynamically load the calendar view
+*/
+
+var monthView = '';
+var months = ['August', 'September', 'October', 'November', 'December', 'January', 'February', 'March', 'April', 'May'];
+$.each(months, function(index, monthName) {
+  monthView += '<div class="month"><h3 class="monthName">' + monthName + '</h3></div>';
+});
+
+$('.calendar').append(monthView);
