b8caa9f06abee605d1f9a9cb19868fe3fa2c9c8f
Sharynne Azhar <sharynneazhar@gmail.com>
2016-09-13 01:03:30 -0500

    add week view of the calendar

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -27,6 +27,10 @@ class Calendar {
     this.createCalendar('month');
     this.populateMonthCalendar();
     $('#month').hide();
+
+    this.createCalendar('week');
+    this.populateWeekCalendar();
+    $('#week').hide();
   }
 
   createCalendar(view) {
@@ -117,5 +121,48 @@ class Calendar {
 
     calendar += '</table>';
     $('#month').html('<div class="month"><h3 class="monthName">' + month.month + '</h3>' + calendar + '</div>');
+  }
+
+  populateWeekCalendar() {
+    let calendar = '<table class="day_container"><tr>' +
+                   '<th>Su</th>' +
+                   '<th>M</th>' +
+                   '<th>Tu</th>' +
+                   '<th>W</th>' +
+                   '<th>Th</th>' +
+                   '<th>F</th>' +
+                   '<th>Sa</th>' +
+                   '</tr>';
+
+    let date = currentDate.getDate() + 1;
+    let dayOfWeek = currentDate.getDay() + 1;
+    let month = months.filter(function(m) {
+      return m.numeric === currentDate.getMonth() + 1;
+    })[0];
+
+    let cellCount = 1;
+    let startOfWeek = date - dayOfWeek;
+    let endOfWeek = date + (7 - dayOfWeek);
+    for (let i = startOfWeek; i < endOfWeek; i++) {
+      let highlight = '';
+      if (currentDate.getMonth() + 1 === month.numeric && currentDate.getDate() === i) {
+        highlight = 'currentDate';
+      }
+
+      if (cellCount === 1) {
+        calendar += '<tr>';
+      }
+
+      calendar += '<td class="' + highlight + '" onclick="popUp(' + i + ')">' + i + '</td>';
+      cellCount++;
+
+      if (cellCount === 8) {
+        calendar += '</tr>';
+        cellCount = 1;
+      }
+    }
+
+    calendar += '</table>';
+    $('#week').html('<div class="week"><h3 class="monthName">' + month.month + '</h3>' + calendar + '</div>');
   }
 }
