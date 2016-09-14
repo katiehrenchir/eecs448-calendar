de808358b658ad114700cd66542c958430ac06e8
Sharynne Azhar <sharynneazhar@gmail.com>
2016-09-14 18:19:45 -0500

    modularize

---
+++ calendarService/calendarStore.php
@@ -0,0 +1,63 @@
+<?php
+
+$server = 'mysql.eecs.ku.edu';
+$username = 'sazhar';
+$password = 'dVBaGrGUnJ24e6uJ';
+$db = 'sazhar';
+
+// Create connection
+$conn = mysql_connect($server, $username, $password);
+mysql_select_db($db);
+
+// Check connection
+if (!$conn) {
+    die('Connection failed: ' . mysql_error());
+}
+
+// Create the events table in database if it doesn't already exist
+$eventData = null;
+$tableName = 'events';
+
+$sql = 'SELECT * FROM events';
+$eventData = mysql_query($sql, $conn);
+
+if (empty($eventData)) {
+    $sql = 'CREATE TABLE IF NOT EXISTS events (
+            event_id INT AUTO_INCREMENT PRIMARY KEY,
+            event_name VARCHAR(20) NOT NULL,
+            event_desc TINYTEXT NOT NULL,
+            start_date DATE NOT NULL,
+            end_date DATE,
+            start_time TIME,
+            end_time TIME)';
+
+    $eventData = mysql_query($sql, $conn);
+
+    if (!$eventData) {
+        die('Could not create table: ' . mysql_error());
+    }
+
+    echo '<br>Created table successfully<br>';
+}
+
+if (!$eventData) {
+    die('Could not get data: '. mysql_error());
+}
+
+$rowDisplay = '<table style="border: thin solid black; text-align: left;"><tr><th>Name</th><th>Description</th><th>Start Date</th><th>End Date</th></tr>';
+while ($row = mysql_fetch_array($eventData)) {
+    $rowDisplay .= '<tr">';
+    $rowDisplay .= '<td>' . $row{'event_name'} . '</td>';
+    $rowDisplay .= '<td>' . $row{'event_desc'} . '</td>';
+    $rowDisplay .= '<td>' . $row{'start_date'} . '</td>';
+    $rowDisplay .= '<td>' . $row{'end_date'} . '</td>';
+    $rowDisplay .= '</tr>';
+}
+$rowDisplay .= '</table>';
+
+echo $rowDisplay;
+echo '<br>Fetched successfully<br>';
+
+mysql_close($conn);
+
+?>
