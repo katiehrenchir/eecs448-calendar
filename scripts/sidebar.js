87eb6563abea4a8619c4df3693e55af7cd825738
Ashli <amosiman93@gmail.com>
2016-09-14 11:46:12 -0500

    added popup for add event

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -22,3 +22,46 @@ class Display {
     this.currentView = view;
   }
 }
+
+var modal = document.getElementById("create");
+
+var btn = document.getElementById("addButton");
+
+var close = document.getElementsByClassName("close")[0];
+
+var startDate = document.getElementsByName("startDate");
+
+var endDate = document.getElementsByName("endDate");
+
+var startTime = document.getElementsByName("startTime");
+
+var endTime = document.getElementsByName("endTime");
+
+var title = document.getElementById("eventTitle");
+
+var description = document.getElementById("eventDesc");
+
+var createEventBtn = document.getElementById("creating");
+
+var eventsList = [];
+
+var eventNum = 0;
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+close.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//not sure if this will store???
+createEventBtn.onclick = function() {
+    eventsList[eventNum] = [title, startDate, endDate, startTime, endTime, description];
+    eventNum++;
+}
