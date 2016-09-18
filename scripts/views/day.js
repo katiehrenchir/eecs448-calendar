215f9af8a80f0ca2289b11374a0e6d71aec7f31a
Ashli Mosiman <a663m213@ku.edu>
2016-09-18 18:12:17 -0500

    Started the next day button

--- scripts/views/day.js
+++ scripts/views/day.js
@@ -1,5 +1,13 @@
 /** Class representing the day */
 class Day {
+
+    constructor()   {
+        //this.displayedDay = {};
+        this.displayNxtDay = true;
+        this.currDayNum = currentDate.getDate();
+        this.currMonthNum = currentDate.getMonth()+1;
+    }
+
   /**
   * Populates the daily calendar view with list of events
   */
@@ -14,5 +22,58 @@ class Day {
     }
     calendar += '</table>';
     $('#day').html('<div class="day"><h3 class="monthName" align="center">' + month.month + ' ' + date + '</h3>' + calendar + '</div>');
+    $('#day .day').prepend(
+      '<a id= "prv_btn" class="btn btn-danger" style="float:left;" onclick="calendar.dayView.showPrevDay()">PREV</a>',
+      '<a id= "nxt_btn" class="btn btn-danger" style="float:right;" onclick="calendar.dayView.showNextDay()">NEXT</a>'
+    );
+
+    //alert(months[this.currMonthNum-1].days);
+//    alert(currentDate.getDate());
+
+    //alert(this.displayedDay.mnth);
+    //alert(getNextMonth(currentMonth));
   }
+
+  /*
+  /Displays the next day, as long as it's not after May 31, 2017
+  */
+  showNextDay() {
+      let theDate = currentDate.toISOString().slice(0,10);
+      document.getElementById("nxt_btn").className = "btn btn-danger disabled";
+     if(this.displayNextDay)    {
+         //reaches May 31st
+         if(this.currMonthNum == 5 && this.currDayNum < month.days) {
+             this.displayNextDay = false;
+         }
+         else if(this.currDayNum != months[this.currMonthNum-1].days)   {
+             alert("hello");
+         }
+    }
+  }
+
+  /*
+  /Displays the previous day,as long as it's not before Aug 1, 2016
+  */
+  showPrevDay() {
+      let theDate = currentDate.toISOString().slice(0,10);
+      document.getElementById("prv_btn").className = "btn btn-danger disabled";
+
+  }
+/*
+  getNextMonth(currentMonth)    {
+      for(var x=0;x <10; x++)   {
+          if(months[x] == currentMonth) {
+              return months[x+1];
+          }
+      }
+  }
+}
+
+getPrevMonth(currentMonth)  {
+    for(var x=0;x <10; x++)   {
+        if(months[x] == currentMonth) {
+            return months[x-1];
+        }
+    }
+}*/
 }
