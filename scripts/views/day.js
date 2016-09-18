9b4c4f30ea94d3761ba90c21227ec18172de4332
erincoots <e.coots218@gmail.com>
2016-09-18 15:36:14 -0500

    Day view now displays test events in correct format

--- scripts/views/day.js
+++ scripts/views/day.js
@@ -4,14 +4,15 @@ class Day {
   * Populates the daily calendar view with list of events
   */
   populateDayCalendar(month, date)  {
-    let calendar = '<table class="day_container"><tr>' + '</tr><tr>';
+    let calendar = '<table class="day_container">';
     let theDate = currentDate.toISOString().slice(0,10);
     for(let x = 0; x < event_num; x++) {
-      if ("2016-09-17" == test_event[x].startDate) {
-        document.write(test_event[x].title);
+      if ("2016-09-18" == test_event[x].startDate) {
+		let evnt = '<h3>' + test_event[x].title + '</h3><p>' + test_event[x].description + '</p><p>' + test_event[x].startTime + ' - ' + test_event[x].endTime + '</p';
+		calendar += '<tr>' + evnt + '</tr>';
       }
     }
-    calendar += '</tr></table>';
+    calendar += '</table>';
     $('#day').html('<div class="day"><h3 class="monthName" align="center">' + month.month + ' ' + date + '</h3>' + calendar + '</div>');
   }
 }
